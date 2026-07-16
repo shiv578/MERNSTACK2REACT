7d40910f66a905dfb56eac0cc318f7b5d14228eb
shiv578 <swarajrkfl22@gmail.com>
2026-07-16 09:54:28 +0530

    learning react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import File1 from "./File1";
+
+function App() {
+  return (
+    <>
+      <File1 />
+    </>
+  );
+}
+
+export default App;
---
+++ src/File1.jsx
@@ -0,0 +1,10 @@
+function File1() {
+  return (
+    <div>
+      <h1>Hello from React Shivam Swaraj</h1>
+      <p>Welcome to your first React component!</p>
+    </div>
+  );
+}
+
+export default File1;
