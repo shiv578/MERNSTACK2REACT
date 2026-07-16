6eb9b5fe00aa86d76c31e0c8f518ed77b2e396f6
shiv578 <swarajrkfl22@gmail.com>
2026-07-16 10:15:25 +0530

    installed taildwind

--- src/File1.jsx
+++ src/File1.jsx
@@ -1,6 +1,7 @@
 function File1() {
   return (
     <div>
+      {/* xml is used to store organise and transport data in a sturctured jhiman readable and machine raadble format */}
       <h1 className= "h1Design">Hello from React Shivam Swaraj</h1>
       <p>Welcome to your first React component!</p>
     </div>
