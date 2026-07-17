c9c5138f478ea060e53100a682712894af91ae5a
shiv578 <swarajrkfl22@gmail.com>
2026-07-17 10:14:04 +0530

    runnig

---
+++ src/Count.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function Count(){
+
+    const[count,setcount] = useState("0");
+ 
+    return(
+<>
+
+<button onClick={() => setcount((count))}></button>
+
+
+</>
+    );
+}
