c6f11efe7ab9c74e2b110be441b55d957ed3e558
shiv578 <swarajrkfl22@gmail.com>
2026-07-20 10:10:08 +0530

    learning use context and useMemo

---
+++ src/Components/Home.jsx
@@ -0,0 +1,7 @@
+function Home(){
+    return(
+<div>This is Home</div>
+    )
+}
+
+export default Home;
--- src/useContext.jsx
+++ src/useContext.jsx
@@ -1,22 +1,32 @@
 import React from 'react'
 
-import { useContext ,createContext} from 'react'
+import { useContext ,createContext,useState} from 'react'
 
 const ThemeContext = createContext();
 
-const useContext = () => {
+function useContext() {
 
-    const{theme,setTheme} = useContext(ThemeContext);
+    // const{theme,setTheme} = useContext(ThemeContext);
 
   return (
     <div>
+
+const [theme,setTheme] = useState("light");
+
+<ThemeContext.Provider value = {(theme,setTheme)}>
+<Home />
+
+</ThemeContext.Provider>
       {/* wer are going to learn use context it is a 
 
       use context is a ract hook used to read data from contect without passing through many layer*/
       
       }
+
+
+
     </div>
-  )
+  );
 }
 
 export default useContext
