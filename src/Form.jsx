7224806eae6d861d275591fb65b6dfa6198826a5
shiv578 <swarajrkfl22@gmail.com>
2026-07-21 10:50:02 +0530

    learning layouts template

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-
+import { useState } from 'react'
 
 
 
 const Form = () => {
+    const[userName,setUsername] =  useState("");
   return (
     <div>
       
-       
+       <input type="text"  value = {userName} />
 
 
     </div>
