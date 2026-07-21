565936b128d552a40cdc01285e9b562b7cb9c275
shiv578 <swarajrkfl22@gmail.com>
2026-07-21 10:46:17 +0530

    learning layouts template

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
+
+
+
 const Form = () => {
   return (
     <div>
       
        
-       
+
 
     </div>
   )
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,6 +4,8 @@ function Layout() {
   return (
     <div>
       
+
+      
     </div>
   )
 }
